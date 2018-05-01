3202568d05682346652e6e222cf3fc3b8cc0b071
Murillo Lopes <mukarillo@gmail.com>
2018-05-01 20:47:45 -0300

    dev

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Element : MonoBehaviour
+{
+    public GameObject colorPrefab;
+    public Transform colorsParent;
+
+    public Image imgTeamLogo;
+
+    private Texture2D tex;
+
+    public void SetupElement(Texture2D teamLogo, List<Color32> colors)
+    {
+        tex = new Texture2D(teamLogo.width, teamLogo.height);
+        tex.SetPixels32(teamLogo.GetPixels32());
+        tex.Apply();
+
+        imgTeamLogo.sprite = Sprite.Create(tex, new Rect(0,0, tex.width, tex.height), Vector2.zero);
+
+        for (int i = 0; i < colors.Count; i++)
+            Instantiate(colorPrefab, colorsParent).GetComponent<Image>().color = colors[i];
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    public List<string> urls = new List<string>(); 
+    public Transform elemenTransformParent;
+    public GameObject elementPrefab;
+    public int maxColors                    = 3;
+    public float colorLimiterPercentage     = 85f;
+    public int uniteColorsTolerance         = 5;
+    public float minimiumColorPercentage    = 10f;
+
+    private Texture2D mTexture;
+
+	private void Start()
+	{
+        mTexture = new Texture2D(2, 2);
+
+        StartCoroutine(ProminentColorsInUrl(urls));
+	}
+
+    private IEnumerator ProminentColorsInUrl(List<string> imgUrls)
+    {
+        for (int i = 0; i < imgUrls.Count; i++)
+        {
+            string imgUrl = imgUrls[i];
+            WWW www = new WWW(imgUrl);
+
+            yield return www;
+            www.LoadImageIntoTexture(mTexture);
+            if (mTexture == null || (mTexture.width == 8 && mTexture.height == 8)) continue;
+
+            //if (mTexture.GetPixel(0, 0).a > 0.99f)
+            //    mTexture = ProminentColor.RemoveBorder(mTexture, Color.white, 20);
+            
+            Instantiate(elementPrefab, elemenTransformParent).GetComponent<Element>().SetupElement(mTexture, ProminentColor.GetColors32FromImage(mTexture, maxColors, colorLimiterPercentage, uniteColorsTolerance, minimiumColorPercentage));
+        }
+    }
+}
